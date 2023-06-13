52edbab19cff3706ba6fbd600a1ddd95cb59d5c0
Pitelom18 <pitelom18@gmail.com>
2023-06-13 11:31:21 -0500

    se suben cambios de componentes estilizados hasta ahora

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
     parserOptions: {
-        parser: '@babel/eslint-parser',
-        requireConfigFile: false,
-    },
-};
+      parser: '@babel/eslint-parser',
+      requireConfigFile: false,
+      ecmaVersion: 6,
+      sourceType: 'module'
+    }
+  };
+  
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GlobalStyle from "./GlobalStyle";
 
 import Container from "./Components/Container";
 import Header from "./Components/Header";
@@ -6,6 +7,7 @@ import Header from "./Components/Header";
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Container />
     </>
